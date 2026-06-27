d1296ca23baeaa4484cdc337aec4b1d498576d06
Abhishek20-prog <Abhishekkumastudent1234@gmail.com>
2026-06-27 11:42:28 +0530

    initial commit

--- ui/src/components/Section1/Navbar.jsx
+++ ui/src/components/Section1/Navbar.jsx
@@ -2,7 +2,7 @@
 
 const Navbar = () => {
   return (
-    <div className="fixed top-0 left-0 right-0 z-[99999] h-16 bg-white/70 backdrop-blur-xl  flex justify-between items-center p-8">
+    <div className="fixed top-0 left-0 right-0 z-99999 h-16 bg-white/70 backdrop-blur-xl  flex justify-between items-center p-8">
     <h4 className="bg-black text-white p-3 rounded-4xl text-[10px] tracking-[.25em] relative z-0">TARGET AUDIENCE</h4>
     <button className="text-[10px] tracking-[.25em]">DIGITAL BANKING PLATFORM</button>
     
